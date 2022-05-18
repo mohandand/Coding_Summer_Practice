a205112ccf13e98d3c989f97fa621d5c52298b07
Mohan Dandigam <mohandand@gmail.com>
2022-05-17 21:52:53 -0700

    Minimum Value in Rotated Sorted Array

---
+++ minimumInRoatedSortedArray.js
@@ -0,0 +1,14 @@
+var findMin = function(nums) {
+    let start=0;
+    let end = nums.length-1;
+    while(start < end){
+        let mid = Math.floor((start+end)/2)
+        if(nums[mid]> nums[end]){
+            start = mid +1
+        }else{
+            end =mid
+        }
+    }
+    return nums[start]
+};
+console.log(findMin([4,5,6,7,0,1,2]))
