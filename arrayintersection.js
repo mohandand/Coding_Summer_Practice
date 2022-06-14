4a16a8374e2e6dfbb61007835e4cf39674664198
Mohan Dandigam <mohandand@gmail.com>
2022-06-13 17:57:28 -0700

    updated the code

--- arrayintersection.js
+++ arrayintersection.js
@@ -1,13 +1,16 @@
 function inter(ar1,ar2){
     let obj1={};
     let res=[];
-   for(let i=0;i<ar1.length;i++){
-    obj1[ar1[i]]? obj1[ar1[i]]++: obj1[ar1[i]]++;
+   for(i of ar1){
+    obj1[i]? obj1[i]++: obj1[i]=1;
    }
-    for(let i=0;i<ar2.length;i++){
-        if(obj1[ar2[i]]){
-            res.push(ar2[i]);
-            obj1[ar2[i]]--;
+    for(k of ar2){
+        if(obj1[k]>0){
+            res.push(k);
+            obj1[k]--;
         }
     }
+    return res
 }
+
+console.log(inter([1,2,2,1],[2,2]))
