4a429c76e03dbb8c9720913daa4d5d59af1bae17
Mohan Dandigam <mohandand@gmail.com>
2022-06-14 12:28:00 -0700

    Sort Colors Program

--- sortColrs.js
+++ sortColrs.js
@@ -1,23 +1,25 @@
-function sortColors(nums){
-   let start=0;
-   let end = nums.length-1;
-   let curr =0;
-   while(start<end){
-    if(nums[start] === 0){
-        let temp = nums[curr];
-        nums[curr] = nums[start];
-        nums[start] = temp; 
-        curr++;
-        start++;
+var sortColors = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let zero = 0;
+    while(left <= right) {
+        if(nums[left] === 0) {
+            let temp = nums[zero];
+            nums[zero] = nums[left];
+            nums[left] = temp;
+            left++;
+            zero++;
+        } else if(nums[left] === 2) {
+            let temp = nums[right];
+            nums[right] = nums[left];
+            nums[left] = temp;
+            right--;
+        } else {
+            left++
+        }
+        
     }
-    else if(nums[start] ===2){
-        let temp = nums[end];
-        nums[end] = nums[start];
-        nums[start] = temp;
-        end--
-    }
-    else{
-        start++
-    }
-   }
-}
+    return nums;
+};
+
+console.log(sortColors([2,2,0,1,2,0]))
