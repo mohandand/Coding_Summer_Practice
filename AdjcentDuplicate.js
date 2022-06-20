f3eb90a1dc79d885494fd3c65ebae5c52896960b
Mohan Dandigam <mohandand@gmail.com>
2022-06-20 14:32:01 -0700

    Coding

--- AdjcentDuplicate.js
+++ AdjcentDuplicate.js
@@ -1,15 +1,22 @@
-function removeDuplicates(array,k){
-    stack = [];
-    for(let i=0;i<array.length;i++){
-        let current = array[i];
-        let peak = stack[stack.length-1];
-        if(stack.legth===0 || peak[0] !== current){
-                stack.push([current,1]);
+function removeDuplicates(s,k){
+    let stack=[];
+    for(let i=0;i<s.length;i++){
+        let current=s[i];
+        let peak = stack[stack.length-1]
+        if(stack.length===0|| peak[0] !==current){
+            stack.push([current,1])
         }else{
-            peek[1]++;
-            if(peek[1]==k){
+            peak[1]++;
+            if(peak[1]===k){
                 stack.pop();
             }
-                }
+        }
+    }
+    let res="";
+
+    for(let [char,count] of stack){
+        res=res+char.repeat(count);
     }
 }
+
+console.log(removeDuplicates("deeedbbcccbdaa",3))
