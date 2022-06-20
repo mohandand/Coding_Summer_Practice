6f393deb963a58a41ddcd4567dfc4e342a51dec0
Mohan Dandigam <mohandand@gmail.com>
2022-06-19 17:35:36 -0700

    Updated Code for My Portfoio roject

--- groupAngrams.js
+++ groupAngrams.js
@@ -3,6 +3,8 @@ function groupAnagrams(arr){
     for(str of arr){
         let sortedStr = str.split('').sort().join("");
         obj[sortedStr] ? obj[sortedStr].push(str) : obj[sortedStr] = [str];
+        console.log(sortedStr);
+        console.log(obj)
     }
     return Object.values(obj)
 }
