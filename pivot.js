8201bba7370de80aeacf98521240ba9e3a65329a
Mohan Dandigam <mohandand@gmail.com>
2022-06-21 18:01:24 -0700

    Coding

--- pivot.js
+++ pivot.js
@@ -1,5 +1,12 @@
 function Pivot(array){
     let sum = array.reduce((p,c) => p+c,0)
-    return sum
+    let leftsum = 0;
+    for(let i=0;i<array.length;i++){
+        let rightsum  = sum-left-array[i];
+        if(rightsum==leftsum){
+            return i
+        }
+        leftsum = leftsum+array[i]
+    }
 }
 console.log(Pivot([1,2,3,2,2,2]))
