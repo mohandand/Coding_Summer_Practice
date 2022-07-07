35af94d0e5ebbaa4fc233118e0f11b0c234dd1e0
Mohan Dandigam <mohandand@gmail.com>
2022-07-07 14:34:18 -0700

    coding practise

--- alphaosition.js
+++ alphaosition.js
@@ -2,10 +2,11 @@ function alphaPos(str){
     let alpha = "abcdefghijklmnopqrstuvwxyz"
     alpha.split('');
     str.replace(/^[a-zA-Z]/).toLowerCase();
+    console.log(str)
     let res=[];
     for( let char of str){
         res.push(str.indexOf(char) + 1);
     }
     return res;
 }
-console.log(alphaPos("bf"))
+console.log(alphaPos("bff$"));
