034731c851aad02da37d2fade63de93036d91958
Mohan Dandigam <mohandand@gmail.com>
2022-07-11 12:53:03 -0700

    Coding Practise

--- BusiestTimeInmal.js
+++ BusiestTimeInmal.js
@@ -3,13 +3,14 @@ function BusiestTime(data){
     let maxcount = 0;
     let maxPeriod=0;
     for(let i=0;i<=data.length;i++){
-        if(data[i][2]==1){
+        let e = data[i][2];
+        if(e === 1){
             count += data[i][1]
         }else{
             count -= data[i][1]
         }
         
-        if(i<n-1 && data[i][0] ==data[i+1][0]){
+        if(i< data.length-1 && data[i][0] ==data[i+1][0]){
             continue
         }
 
@@ -22,3 +23,13 @@ function BusiestTime(data){
 
     return maxPeriod;
 }
+
+console.log(BusiestTime([ [1487799425, 14, 1], 
+    [1487799425, 4,  0],
+    [1487799425, 2,  0],
+    [1487800378, 10, 1],
+    [1487801478, 18, 0],
+    [1487801478, 18, 1],
+    [1487901013, 1,  0],
+    [1487901211, 7,  1],
+    [1487901211, 7,  0] ]))
