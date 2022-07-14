883d75a7a00dd280c15760dbf926d8658d6ef011
Mohan Dandigam <mohandand@gmail.com>
2022-07-14 16:22:20 -0700

    JS Coding Practise

---
+++ revInt.js
@@ -0,0 +1,12 @@
+function revInt(num){
+    let res = 0;
+    while(num>0){
+        let last = num % 10
+
+        res = res *10 +last;
+
+        num = Math.floor(num/10);
+    }
+
+    return res;
+}
