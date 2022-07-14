4a2a7f181673a903574d8633bdd52bd91112166f
Mohan Dandigam <mohandand@gmail.com>
2022-07-14 12:11:17 -0700

    JS Coding Practise

---
+++ validParnthesis.js
@@ -0,0 +1,24 @@
+function parMatch(s){
+    let obj={
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+
+    let stack = [];
+
+    for(let char of s){
+        if(char === '(' || char ==='{' ||char==='['){
+            stack.push(char)
+        }else{
+            if(obj[stack.pop()] !== char){
+                return false;
+            }
+        }
+    }
+    if(stack.length>0){
+        return false
+    }else{
+        return true;
+    }
+}
