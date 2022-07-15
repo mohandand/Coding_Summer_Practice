f8cdff754529b22f50a1e012b7634ca4d272c93f
Mohan Dandigam <mohandand@gmail.com>
2022-07-15 16:22:35 -0700

    JS Coding Practise

--- quaterPlet.js
+++ quaterPlet.js
@@ -1,12 +1,12 @@
 function findArrayQuadruplet(arr, s) {
     let n = arr.length;
-    if (n < 4) return [];
         arr.sort((a,b)=>a-b)  
-    for(let i=0;i<n-4;i++){
-      for(let j = i+1;j<n-3;j++){
+    for(let i=0;i<n-3;i++){
+      for(let j = i+1;j<n-2;j++){
          low = j + 1, high = n - 1;
         while(low<high){
           let sum = arr[low]+arr[high]+arr[i]+arr[j];
+          console.log(sum);
           if(sum==s){
             return [arr[i], arr[j], arr[low], arr[high]]
           }else if(sum < s){
