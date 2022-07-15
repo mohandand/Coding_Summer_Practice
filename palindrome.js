f5c3cb1d14e283ed4c94fc84ec6add138060a106
Mohan Dandigam <mohandand@gmail.com>
2022-07-14 17:49:56 -0700

    JS Coding Practise

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
 function palindrome(str){
     let res= '';
     for(let i=str.length-1;i>=0;i--){
+        console.log(str[i])
         res += str[i];
     }
     return res==str
-
 }
 
 console.log(palindrome("madam"))
