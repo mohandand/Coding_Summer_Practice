0920719d1143349746c8789ef8c6a64d3091750c
Mohan Dandigam <mohandand@gmail.com>
2022-07-21 21:52:00 -0700

    JS Coding Practise

--- decyrptMessage.js
+++ decyrptMessage.js
@@ -3,7 +3,7 @@ https://www.pramp.com/challenge/8noLWxLP6JUZJ2bA2rnx
 Convert every letter to its ASCII value. Add 1 to the first letter, and then for every letter from the second one to the last one, add the value of the previous letter. Subtract 26 from every letter until it is in the range of lowercase letters a-z in ASCII. Convert the values back to letters.
 
 For instance, to encrypt the word “crime”
-
+de
 Decrypted message:	c	r	i	m	e
 Step 1:	99	114	105	109	101
 Step 2:	100	214	319	428	529
