4e1e0bc1ab8cdd5ff5743ad8dddcaad951294f41
Mohan Dandigam <mohandand@gmail.com>
2022-08-14 14:33:52 -0700

    Javscrit Code Practice

--- mergeInterval.js
+++ mergeInterval.js
@@ -1,23 +1,23 @@
 /* 
+first take first pair onto array and commpare with rest of the elements
 main logic is comapring below 
 if overlas merge it other wise push inteval
 Ex:1 [1,2] [1,4]  2>=1  so change end point to [1,4]
-Ex:2 [1,3],[2,6]   3>=2 so change end point to [1,6]
-
+Ex:2 [1,3],[2,6]  3>=2 so change end point to [1,6]
 */
 
 function mergeInterval(intervals){
     intervals.sort((a,b) => a[0]-b[0])
 
     let res = [intervals[0]]
-    for(let interval of intervals){
+    for(let i=1;i<intervals.length;i++){
         let end1 = res[res.length-1][1];
-        let start2 = interval[0];
-        let end2 = interval[1];
+        let start2 = intervals[i][0];
+        let end2 = intervals[i][1];
         if(end1>=start2){
             res[res.length-1][1] =Math.max(end1,end2)
         }else{
-            res.push(interval)
+            res.push(intervals[i])
         }
      }
      return res;
