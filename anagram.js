f27e3b1f8abadbae4a74180d45c81b8168701116
Mohan Dandigam <mohandand@gmail.com>
2022-08-15 16:08:31 -0700

    Javscrit Code Practice

--- anagram.js
+++ anagram.js
@@ -1,7 +1,17 @@
-function anagram(str){
-    let array = new Array(2).fill(0);
-    console.log(array);
+function anagram(str1,str2){
+    let array = new Array(256).fill(0);
+    if(str1.length !== str2.length) return false
 
+    for(let i = 0;i<str1.length;i++){
+        array[str1.charCodeAt(i)]++;
+        array[str2.charCodeAt(i)]--;
+    }
+    for(let i=0;i<str1.length;i++){
+        if(array[i] !==0){
+            return false;
+        }
+    }
+    return true
 }
 
-console.log(anagram());
+console.log(anagram("listen","netsli"));
