185132785394799a99429d327c9e93667c8d44b6
Mohan Dandigam <mohandand@gmail.com>
2022-08-24 18:36:27 -0700

    Grokking Pattern Coding Practice

--- nonoverlapping.js
+++ nonoverlapping.js
@@ -1,19 +1,6 @@
-function nonoverLapping(intervals){
-    let count = 0;
+/* 
+arra = [1,0,2,0]
 
-    intervals.sort((a, b) => a[1] - b[1]);
+lets 
 
-    let previous = intervals[0];
-
-    for (let i = 1; i < intervals.length; i++) {
-        if (intervals[i][0] < previous[1]) {
-            count += 1;
-        } else {
-            previous = intervals[i];
-        }
-    }
-
-    return count;
-}
-
-console.log(nonoverLapping([[1,2],[2,3],[3,4],[1,3]]))
+*/
